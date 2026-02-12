998780353d60289a5c6246eb780409fd0ea636f2
Leonel Molina <satelitehospital@gmail.com>
2026-02-11 18:55:06 -0600

    creacion de cotegoria y funcion de poblado

--- src/controllers/externalController.js
+++ src/controllers/externalController.js
@@ -9,21 +9,39 @@ const poblarProductos = async (request, response) => {
         let inserciones = 0;
         // Destructurar el objeto
         for(const product of products){
-            const { title, price, description, image} = product;
+            
+            const { title, price, description, image, category} = product;
 
             const stock = Math.floor(Math.random() * 50) + 1;
+
+            // --- INICIO LÓGICA CATEGORÍA ---
+            let id_categoria;
+            
+            // Verificar si la categoría ya existe
+            const catResult = await pool.query('SELECT id FROM Categoria WHERE nombre = $1', [category]);
+
+            if (catResult.rows.length > 0) {
+                // Si existe, tomamos el ID
+                id_categoria = catResult.rows[0].id;
+            } else {
+                // Si no existe, la insertamos y devolvemos el ID creado
+                const newCat = await pool.query('INSERT INTO Categoria (nombre) VALUES ($1) RETURNING id', [category]);
+                id_categoria = newCat.rows[0].id;
+            }
+            // --- FIN LÓGICA CATEGORÍA ---
 
             const result = await pool.query('SELECT * FROM productos');
             
             console.log(result.rows);
 
+            
             const query = `
                 INSERT INTO productos
-                (nombre, precio, stock, descripcion, imagen_url)
-                VALUES ($1, $2, $3, $4, $5)
+                (nombre, precio, stock, descripcion, imagen_url, id_categoria)
+                VALUES ($1, $2, $3, $4, $5, $6)
             `
 
-            await pool.query(query, [title, price, stock, description, image]);
+            await pool.query(query, [title, price, stock, description, image, id_categoria]);
 
             inserciones++;
         }
