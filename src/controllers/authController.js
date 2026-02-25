57c94e70876c38f5908eafcd1bb5917f5aa9d862
Leonel Molina <satelitehospital@gmail.com>
2026-02-25 13:37:58 -0600

    Instalacion de jsonwebtoken

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,39 +1,75 @@
 const pool = require('../config/db');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const register = async (request, response) => {
 
     const { email, password } = request.body;
 
     try {
-    const userExists = await pool.query('SELECT * FROM usuarios WHERE email = $1', [email]);
+        const userExists = await pool.query('SELECT * FROM usuarios WHERE email = $1', [email]);
 
-    if (userExists.rows.length > 0) {
-        return response.status(400).json({ error: 'El usuario ya existe' });
-    }       
+        if (userExists.rows.length > 0) {
+            return response.status(400).json({ error: 'El usuario ya existe' });
+        }       
 
-    const salt = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(password, salt);
+        const salt = await bcrypt.genSalt(10);
+        const passwordHash = await bcrypt.hash(password, salt);
 
-    const newUser = await pool.query(
-        'INSERT INTO usuarios (email, password) VALUES ($1, $2) RETURNING id, email',
-        [email, passwordHash]
-    );
-    
-    response.status(201).json({
-        message: 'Usuario registrado exitosamente',
-        user: newUser.rows[0]
-    });
+        const newUser = await pool.query(
+            'INSERT INTO usuarios (email, password) VALUES ($1, $2) RETURNING id, email',
+            [email, passwordHash]
+        );
 
-} catch (error) {
-    console.error(error);
-    response.status(500).json({ error: "Error en el servidor" });
-}
-}
+        response.status(201).json({
+            message: 'Usuario registrado exitosamente',
+            user: newUser.rows[0]
+        });
+    } catch (error) {
+        console.error(error);
+        response.status(500).json({ error: "Error en el servidor" });
+    }
+};
+
+const login = async (request, response) => {
+    const { email, password } = request.body;   
+    try {
+        const user = await pool.query('SELECT * FROM usuarios WHERE email = $1', [email]);
+        if (user.rows.length === 0) {
+            return response.status(400).json({ error: 'Credenciales Invalidas (Email)' });
+        }
+
+        const usuario = user.rows[0];
+
+        const isMatch = await bcrypt.compare(password, usuario.password);
+
+        if (!isMatch) {
+            return response.status(400).json({ error: 'Credenciales Invalidas (Password)' });
+        }
+
+        const payload = {
+            id: usuario.id,
+            rol: usuario.rol,
+            email: usuario.email
+        };
+
+        const token = jwt.sign(
+            payload,
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' }
+        );
+
+        response.json({ token });
+    } catch (error) {
+        console.error(error);
+        response.status(500).json({ error: "Error en el servidor" });
+    }
+};
 
 
 
 
 module.exports = {
-    register
+    register,
+    login
 };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { register } = require('../controllers/authController');
+const { login } = require('../controllers/authController');
 
 router.post('/register', register);
+router.post('/login', login);
 // router.post('/login', authController.login);
 
 module.exports = router;
